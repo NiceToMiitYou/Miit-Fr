66a4b05d01bf919daab2f8720237e20a17618c45
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-04-10 23:04:45 +0200

    [ENHANCEMENT] Add support for translations in JavaScript (using MiitTranslator) - MiitTranslator.get('key', 'domain'); => Cf: http://symfony.com/doc/current/components/translation/usage.html

---
+++ src/Miit/FrontendBundle/Controller/TranslationController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Miit\FrontendBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TranslationController extends Controller
+{
+    /**
+     * @Route("/dist/js/translations.js", name="translations_script")
+     */
+    public function translationsAction(Request $request)
+    {
+        $params = array(
+            'messages' => $this->get('translator')->getMessages()
+        );
+
+        $rendered = $this->renderView('MiitFrontendBundle::translations.js.twig', $params);
+        
+        // Instanciate the response
+        $response = new Response($rendered);
+
+        // This is Javascript
+        $response->headers->set('Content-Type', 'application/javascript');
+
+        return $response;
+    }
+}
