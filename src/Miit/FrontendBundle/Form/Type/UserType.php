d3ddabca5283365f0e470faaebc78f20acb58701
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-04-12 16:27:31 +0200

    [FIX] Various changes

--- src/Miit/FrontendBundle/Form/Type/UserType.php
+++ src/Miit/FrontendBundle/Form/Type/UserType.php
@@ -19,17 +19,7 @@ class UserType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('username', 'text');
-        
         $builder->add('email',    'email');
-
-        $builder->add('password', 'repeated', array(
-            'type' => 'password',
-            'invalid_message' => 'Les mots de passe doivent correspondre',
-            'options' => array('required' => true),
-            'first_options'  => array('label' => 'Mot de passe'),
-            'second_options' => array('label' => 'Mot de passe (validation)'),
-        ));
     }
 
     /**
