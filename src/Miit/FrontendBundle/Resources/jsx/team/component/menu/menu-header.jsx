886a4e5dd2721706b747b5ac919d49925c701cef
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-05-03 20:15:49 +0200

    [ENHANCEMENT] Slice menu

---
+++ src/Miit/FrontendBundle/Resources/jsx/team/component/menu/menu-header.jsx
@@ -0,0 +1,10 @@
+MiitComponents.MenuHeader = React.createClass({
+    render: function() {
+        return (
+            <a className="miit-component menu-header sl-header bg-blue-grey darken-1">
+                <i className="fa fa-bars pull-left"></i>
+                Menu
+            </a>
+        );
+    }
+});
---
+++ src/Miit/FrontendBundle/Resources/jsx/team/component/menu/menu-label.jsx
@@ -0,0 +1,14 @@
+MiitComponents.MenuLabel = React.createClass({
+    render: function() {
+        var cx = React.addons.classSet;
+        
+        var classes = cx('pull-left', 'fa', this.props.icon || '');
+        
+        return (
+            <span className="miit-component menu-label sl-label">
+                <i className={classes}></i>
+                {this.props.label}
+            </span>
+        );
+    }
+});
