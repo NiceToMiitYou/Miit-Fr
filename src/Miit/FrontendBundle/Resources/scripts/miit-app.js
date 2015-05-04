e6f6708a34f0ec45d585c1dfab9abee811e90c27
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-05-05 00:00:08 +0200

    [ENHANCEMENT] Initialize the router

--- src/Miit/FrontendBundle/Resources/scripts/miit-app.js
+++ src/Miit/FrontendBundle/Resources/scripts/miit-app.js
@@ -2,14 +2,19 @@ window.MiitApp = (function() {
     var MiitApp = injector.resolve(
         ['miit-utils', 'miit-storage', 'miit-router', 'miit-request'],
         function(MiitUtils, MiitStorage, MiitRouter, MiitRequest) {
-            var VERSION = '0.0.0';
+            var VERSION   = '0.0.0';
+            var COPYRIGTH = 'All rigths reserved to ITEvents.'
+
+            // Initiliaze the router
+            MiitRouter.init();
 
             return {
-                VERSION: VERSION,
-                request: MiitRequest,
-                router:  MiitRouter,
-                storage: MiitStorage,
-                utils:   MiitUtils
+                COPYRIGTH: COPYRIGTH,
+                VERSION:   VERSION,
+                request:   MiitRequest,
+                router:    MiitRouter,
+                storage:   MiitStorage,
+                utils:     MiitUtils
             };
         }
     );
