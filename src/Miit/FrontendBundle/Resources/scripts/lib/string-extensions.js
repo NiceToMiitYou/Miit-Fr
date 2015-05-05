1e0f840076c5563f647acd6efb7526a469f91fd2
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-05-05 20:08:13 +0200

    [ENHANCEMENT] Add functions for strings

---
+++ src/Miit/FrontendBundle/Resources/scripts/lib/string-extensions.js
@@ -0,0 +1,11 @@
+(function(){
+    String.prototype.capitalize = function(){
+        return this.toLowerCase().replace(/\b\w/g, function (m) {
+            return m.toUpperCase();
+        })
+    };
+
+    String.prototype.dashToCapitalize = function(){
+        return this.replace(/_/g, ' ').capitalize().replace(/ /g, '');
+    };
+})();
