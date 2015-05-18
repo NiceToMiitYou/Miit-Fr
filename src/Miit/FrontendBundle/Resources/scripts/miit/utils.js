c1b6af77c7f105b8273ea4d1bf118219b4688fd7
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-05-18 20:05:48 +0200

    [ENHANCEMENT] Add guid generator

--- src/Miit/FrontendBundle/Resources/scripts/miit/utils.js
+++ src/Miit/FrontendBundle/Resources/scripts/miit/utils.js
@@ -27,6 +27,14 @@
                 };
             };
 
+            var guidGenerator = function() {
+                function s4() {
+                    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+                }
+
+                return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+            };
+
             /**
              * Ajax part
              */
@@ -126,6 +134,10 @@
                     password: validatorGenerator(RegexPassword),
                     team:     validatorGenerator(RegexTeamName),
                     user:     validatorGenerator(RegexUserName)
+                },
+
+                generator: {
+                    guid: guidGenerator
                 }
             };
         }
